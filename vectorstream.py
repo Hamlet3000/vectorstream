a8f56f5a94963082eff4cac5d4229d103fe5e02e
Hamlet3000 <75612866+Hamlet3000@users.noreply.github.com>
2022-04-25 19:39:27 +0200

    Unneeded code removed

--- vectorstream.py
+++ vectorstream.py
@@ -2,107 +2,85 @@
 
 import io
 import sys
-import time
-from lib import flask_helpers
 import anki_vector
 
 ###############################################################################
 try:
-    from flask import Flask, request
+   from flask import Flask, Response
 except ImportError:
-    sys.exit("Cannot import from flask: Do `pip3 install --user flask` to install")
-
-try:
-    from PIL import Image, ImageDraw
-except ImportError:
-    sys.exit("Cannot import from PIL: Do `pip3 install --user Pillow` to install")
-
-
-###############################################################################
-# Create a place-holder PIL image to use until we have a live feed from Vector"""
-def create_default_image(image_width, image_height):
-    image_bytes = bytearray([0x70, 0x70, 0x70]) * image_width * image_height
-    image = Image.frombytes('RGB', (image_width, image_height), bytes(image_bytes))
-    return image
+   sys.exit("Cannot import from flask: Do `pip3 install --user flask` to install")
 
 flask_app = Flask(__name__)
-_default_camera_image = create_default_image(320, 240)
-
 
 ###############################################################################
-def get_annotated_image():
-    image = flask_app.vectorstream.vector.camera.latest_image
-    return image.annotate_image()
-
-###############################################################################
-# Runs a endless loop
+# runs a endless loop
 def run_loop():
-    while True:
 
-        # Video streaming generator function
-        if flask_app.vectorstream:
-            image = get_annotated_image()
+   while True:
+      # Video streaming generator function
+      if flask_app.vectorstream:
+         image_raw = flask_app.vectorstream.vector.camera.latest_image
+         image = image_raw.annotate_image()
+         img_io = io.BytesIO()
+         image.save(img_io, 'JPEG')
+         img_io.seek(0)
 
-            img_io = io.BytesIO()
-            image.save(img_io, 'PNG')
-            img_io.seek(0)
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/png\r\n\r\n' + img_io.getvalue() + b'\r\n')
-        else:
-            pass
-        
-         time.sleep(.1)
+         yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + img_io.getvalue() + b'\r\n')        
+      else:
+         pass
 
 ###############################################################################
-# Erzeugt website unter 127.0.0.1:5000
+# creates website 127.0.0.1:5000
 @flask_app.route("/")
 def handle_index_page():
-    return """
-    <html>
-        <body>
-            <img src="vectorImage" id="vectorImageId" width=100%>
-        </body>
-    </html>
-    """
+   return """
+   <html>
+      <body>
+         <img src="video" id="vectorVideoId" width=100%>
+      </body>
+   </html>
+   """
 
 ###############################################################################
-@flask_app.route("/vectorImage")
-def handle_vectorImage():
-    return flask_helpers.stream_video(run_loop)
+@flask_app.route("/video")
+def video_feed():
+   return Response(run_loop(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 ###############################################################################
-class VectorStream:
-    def __init__(self, robot):
-        self.vector = robot
+class vector_stream:
+   def __init__(self, robot):
+      self.vector = robot
+
 
 ###############################################################################
-def runFlask(robot):
-    flask_app.vectorstream = VectorStream(robot)
-    flask_app.display_debug_annotations = 1
-    flask_app.run(
-        host="192.168.0.50",
-        port=5000
-    )
-    #flask_helpers.run_flask(flask_app)
+def run_flask(robot):
+   flask_app.vectorstream = vector_stream(robot)
+   flask_app.run(
+      host="192.168.0.50",
+      port=5000
+   )
 
 ###############################################################################
 def main():
 
-    args = anki_vector.util.parse_command_args()
+   args = anki_vector.util.parse_command_args()
 
-    with anki_vector.Robot(args.serial, 
-                           enable_face_detection=True, 
-                           enable_custom_object_detection=True) as robot:
+   with anki_vector.Robot(args.serial, 
+                          enable_face_detection=True, 
+                          cache_animation_lists=False,
+                          enable_custom_object_detection=True) as robot:
 
-        # open camera, and release control
-        robot.camera.init_camera_feed()
-        robot.behavior.say_text("I restart my camera!")
-        robot.conn.release_control()
+      # open camera and release control
+      robot.camera.init_camera_feed()
+      #robot.behavior.say_text("I'm restart my camera!")
+      robot.audio.stream_wav_file('/home/ubuntu/Apps/Vectorstream/vector_bell_whistle.wav')
+      robot.conn.release_control()
 
-        # Flask
-        runFlask(robot)
-
+      # Flask
+      run_flask(robot)
+     
 ###############################################################################
 if __name__ == "__main__":
-    main()
+   main()
 
